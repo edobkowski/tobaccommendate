cda4830a9b7dc1a7c520bc675f410ebdcea5833b
edobkowski <edobkowski@gmail.com>
2018-10-27 18:51:38 +0200

    Implement Country entity class

---
+++ src/main/java/com/tobaccode/tobaccommendate/entities/Country.java
@@ -0,0 +1,44 @@
+package com.tobaccode.tobaccommendate.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "countries")
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    public Country() {
+    }
+
+    public Country(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
