456995f19d184ee3267496d9d6c1cd1b4e7fe665
edobkowski <edobkowski@gmail.com>
2018-10-27 16:37:21 +0200

    Initial commit

---
+++ src/main/java/com/tobaccode/tobaccommendate/TobaccommendateApplication.java
@@ -0,0 +1,12 @@
+package com.tobaccode.tobaccommendate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobaccommendateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TobaccommendateApplication.class, args);
+	}
+}
